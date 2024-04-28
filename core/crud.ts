f5ab80389a9509fc911394d5ef47ed2107605ffc
Rodrigo Finkler <finklerrg@gmail.com>
2024-04-27 22:32:51 -0300

    Added uuids to the TODOS and added update() functions

--- core/crud.ts
+++ core/crud.ts
@@ -1,4 +1,5 @@
 import fs from "fs"; // ES6
+import { v4 as uuid } from "uuid";
 // const fs = require("fs"); -- CommonJS
 
 const DB_FILE_PATH = "./core/db";
@@ -6,13 +7,15 @@ const DB_FILE_PATH = "./core/db";
 console.log("[CRUD]");
 
 interface Todo {
+  id: string;
   date: string;
   content: string;
   done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
   const todo: Todo = {
+    id: uuid(),
     date: new Date().toISOString(),
     content: content,
     done: false,
@@ -31,7 +34,7 @@ function create(content: string) {
       2
     )
   );
-  return content;
+  return todo;
 }
 
 function read(): Array<Todo> {
@@ -44,6 +47,35 @@ function read(): Array<Todo> {
   return parsed_res.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+    if (isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo);
+    }
+  });
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+  if (!updatedTodo) {
+    throw new Error("Please, provide another ID");
+  }
+  return updatedTodo;
+}
+
+function updateContentById(id: string, content: string): Todo {
+  return update(id, { content });
+}
+
 function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH, "");
 }
@@ -52,4 +84,10 @@ function CLEAR_DB() {
 CLEAR_DB();
 create("Primeira TODO");
 create("Segunda TODO");
+const thirdTodo = create("TODO 3");
+update(thirdTodo.id, {
+  content: "Terceira TODO atualizada",
+  done: true,
+});
+updateContentById(thirdTodo.id, "Terceira TODO + atualizada");
 console.log(read());

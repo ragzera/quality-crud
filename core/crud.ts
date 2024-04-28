980ee2019ed13f86219a2d2a91d564b6055c7903
Rodrigo Finkler <finklerrg@gmail.com>
2024-04-27 21:41:14 -0300

    Implementation of the read() function

--- core/crud.ts
+++ core/crud.ts
@@ -5,11 +5,51 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
 function create(content: string) {
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos: Array<Todo> = [...read(), todo];
+
   // save content in a file
-  fs.writeFileSync(DB_FILE_PATH, content);
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
   return content;
 }
 
+function read(): Array<Todo> {
+  // read content from a file
+  const db_res = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const parsed_res = JSON.parse(db_res || "{}");
+  if (!parsed_res?.todos) {
+    return [];
+  }
+  return parsed_res.todos;
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
+
 // [SIMULATION]
-console.log(create("Eu vou usar pnpm em vez de npm!!!"));
+CLEAR_DB();
+create("Primeira TODO");
+create("Segunda TODO");
+console.log(read());
